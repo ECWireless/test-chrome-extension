b78aeeb0c5a28bd38443971b8fc2eb5b2bb65dc2
ECWireless <Elliott@valtechcreative.com>
2021-01-24 13:47:09 -0700

    Add snowflakes

--- public/content.js
+++ public/content.js
@@ -1,6 +1,15 @@
-// This file is injected as a content script
-const header = document.createElement("h1");
-header.innerHTML = "Christmas!";
-
 const body = document.getElementsByTagName("body");
-body[0]?.prepend(header);
+
+const snowflakesContainer = document.createElement("div");
+snowflakesContainer.className = "snowflakes";
+snowflakesContainer.setAttribute("aria-hidden", "true");
+
+const snowflake = document.createElement("div");
+snowflake.className = "snowflake";
+snowflake.innerHTML = "❆";
+
+for (let i = 0; i < 12; i++) {
+  snowflakesContainer.appendChild(snowflake.cloneNode(true));
+}
+
+body[0]?.prepend(snowflakesContainer);
